4c3e3144b89af4402600b852e4fb0cf0685f94da
lindream88 <54275054+lindream88@users.noreply.github.com>
2019-08-26 21:29:31 +0800

    導航完成

--- Web/artificial_intelligence_ambulance_system/ambulance_system/views.py
+++ Web/artificial_intelligence_ambulance_system/ambulance_system/views.py
@@ -82,14 +82,21 @@ def index(request):
 def schedule(request,id):
 
 	car_accident=TrafficConditionLog.objects.get(id=id)
-	
+
 	begin_latitude = car_accident.ResponsibleUnit.Location.Latitude
 	begin_longitude = car_accident.ResponsibleUnit.Location.Longitude
 	finish_latitude = car_accident.Monitor.Location.Latitude
 	finish_longitude = car_accident.Monitor.Location.Longitude
 
-	#return render(request,"home.html",locals())
-	return render(request,"schedule.html",locals())
+#	begin_latitude = 25.0068
+#	begin_longitude = 121.5233
+#	finish_latitude = 25.1234
+#	finish_longitude = 121.4321
+	start = str(begin_latitude)+','+str(begin_longitude)
+	end = str(finish_latitude)+','+str(finish_longitude)
+#	start='25,121'
+#	end='26.3,121'
+	return render(request,"schedule2.html",locals())
 
 def logout(request):
 	django_logout(request)
